b0624b5d7bfca2849e598a15a7b35e204949f232
qiuyanjun888 <qiuyanjun888@gmail.com>
2026-01-03 15:19:23 +0800

    chore: remove demo codes and aglin the backend and frontend version

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { PrismaService } from './prisma.service'
 import { UsersModule } from './system/users/users.module'
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
